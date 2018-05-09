204f6b228c37e014f894ebcbbc2bd5fa16b57981
twknab <20636750+twknab@users.noreply.github.com>
2018-05-08 20:21:52 -0700

    added some basic styles and some mockup content

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,3 +1,3 @@
-<p>Copying is the best form of flattery.</p>
+<p class="footer-text">No Copyright. "Imitation is the sincerest form of flattery" - Oscar Wilde</p>
 </body>
 </html>
--- application/views/index.php
+++ application/views/index.php
@@ -9,7 +9,12 @@ $data["description"] = "This is my website description.";
 $this->load->view('includes/header', $data);
 ?>
 <!-- Begin Body Content  -->
-<h1>This is a test.</h1>
+<h1>Welcome,</h1>
+<p>Thank you for your visit and may you enjoy your stay.</p>
+<p>Explore my <a href="#">web projects</a>, <a href="#">graphic designs</a> or <a href="#">writings</a>. The latest and greatest is released via the <a href="#">blog</a>.</p>
+<p>Please <a href="#">contact</a> me if you require my services, or if you have a suggestion for improving this website or any project.</p>
+<p>Thank you and enjoy your visit,</p>
+<p>Tim Knab</p>
 <?php 
 // Load footer:
 $this->load->view('includes/footer');
